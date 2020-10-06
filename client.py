aabae15a564a0b15901ca98bda272dc8aff64a37
mariaeduardaoer <mariaeduardaoer27@gmail.com>
2020-10-05 22:39:27 -0300

    comeco do chat

--- client.py
+++ client.py
@@ -1,1 +1,13 @@
+import socket
+
+HOST = 'localhost' # servidor que quero conectar
+PORT = 12321
+
+mensagem = input('Digite uma mensagem a ser enviada ao servidor: ')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(mensagem.encode('utf-8')) # manda o array de bytes da string
+    data = s.recv(1024)
+    print(f'Recebido {data.decode("utf-8")}')
 
--- server.py
+++ server.py
@@ -1,1 +1,22 @@
+import socket
 
+HOST = ''
+PORT = 12321
+
+# with: no final da execução vai chamar o close
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # bind: cria o vínculo
+    s.bind((HOST, PORT))
+    # listen: quantas conexões pendentes existe no socket
+    s.listen(5)  # até 5 em espera, se chegar outra (a sexta) é rejeitada
+    print(f"Aguardando novas conexões...")
+    conn, addr = s.accept()
+
+    with conn:
+        print(f'Conectado a {addr}')
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            if len(data) > 0:
+                print(f"Mensagem recebida: {data.decode('utf-8')}")
+            conn.sendall(data)
