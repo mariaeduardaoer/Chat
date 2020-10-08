952b4d03f086fdef65bc402e8035ea3ab1b34a5d
mariaeduardaoer <mariaeduardaoer27@gmail.com>
2020-10-07 22:32:11 -0300

    thread

--- client.py
+++ client.py
@@ -1,13 +1,27 @@
 import socket
+import threading
 
 HOST = 'localhost' # servidor que quero conectar
 PORT = 12321
 
-mensagem = input('Digite uma mensagem a ser enviada ao servidor: ')
+def handle_received_message(sock):
+    while True:
+        data = sock.recv(1024)
+        print(" ")
+        print(f'Received {data.decode("utf-8")}')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(mensagem.encode('utf-8')) # manda o array de bytes da string
-    data = s.recv(1024)
-    print(f'Recebido {data.decode("utf-8")}')
+    s.connect((HOST, PORT))  # faz a conexão
+    
+    thread = threading.Thread(target=handle_received_message, args=(s,))
+    thread.start()
 
+    while True:
+        try:
+            mensagem = input('Digite uma mensagem a ser enviada ao servidor: ')
+            s.sendall(mensagem.encode('utf-8')) # manda o array de bytes da string
+        except KeyboardInterrupt:
+            print(" ")
+            print('Encerrando cliente')
+            s.close()
+            break
--- server.py
+++ server.py
@@ -1,7 +1,22 @@
 import socket
+import threading
 
 HOST = ''
 PORT = 12321
+
+clients = []
+
+def client_handle(conn, addr):
+  with conn:
+        print(f'Conectado a {addr}')
+        print(" ")
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            print(f"Mensagem recebida de {addr}: {data.decode('utf-8')}")
+            for client in clients:
+                if addr != client["addr"]:
+                    client["conn"].sendall(data) # envia de volta para o cliente a mesma mensagem que ele enviou
 
 # with: no final da execução vai chamar o close
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -9,14 +24,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     # listen: quantas conexões pendentes existe no socket
     s.listen(5)  # até 5 em espera, se chegar outra (a sexta) é rejeitada
-    print(f"Aguardando novas conexões...")
-    conn, addr = s.accept()
+    
+    while True:
+        try:
+            print(f"Aguardando novas conexões...")
+            conn, addr = s.accept()
+            clients.append({"connection": conn, "address": addr})
 
-    with conn:
-        print(f'Conectado a {addr}')
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            if len(data) > 0:
-                print(f"Mensagem recebida: {data.decode('utf-8')}")
-            conn.sendall(data)
+            thread = threading.Thread(target=client_handle, args=(conn, addr,)) # "," é pra saber que é o fim da tupla
+            thread.start()
+        except KeyboardInterrupt:
+            print("Encerrando servidor.")
+            s.close()
+            break  
